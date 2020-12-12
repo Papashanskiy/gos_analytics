f76bd2e568c11dda291f0f891409aa4f7694abb4
Dmitriy Apashanskiy <apashanskiy@gmail.com>
2020-12-12 22:05:23 +0300

    Добавлена вьюшка в апи для возвращения маски карты

--- services/web/project/api/routes.py
+++ services/web/project/api/routes.py
@@ -1,5 +1,15 @@
 from flask import Blueprint, jsonify, request
+import click
+
+from project.utils.map_mask_database import MapMask
+from project.utils.rf_regions import rf_regions
 
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
+
+@api.route('/map_mask', methods=['GET'])
+def get_map_mask():
+    # map_mask = MapMask.get_mask()
+    map_mask = rf_regions
+    return jsonify(map_mask=map_mask)
